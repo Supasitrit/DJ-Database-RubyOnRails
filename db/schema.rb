bc2b8d0f12f1afb4cd43de36100f76db277ded47
Supasitrit <supasit.rit@gmail.com>
2015-03-12 14:48:38 +0700

    done paperclip for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309204225) do
+ActiveRecord::Schema.define(version: 20150312045746) do
 
   create_table "commontator_comments", force: :cascade do |t|
     t.string   "creator_type"
@@ -91,6 +91,10 @@ ActiveRecord::Schema.define(version: 20150309204225) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
