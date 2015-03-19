22f2321a37088894044cc2406af757351e5ec14d
supasit <supasit.rit@gmail.com>
2015-03-19 21:30:26 +0700

    Dj Sohum

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,6 +1,7 @@
 class StaticPageController < ApplicationController
   def home
-  	@djs = Dj.paginate(:page => params[:page], :per_page => 4)
+  	# @djs = Dj.paginate(:page => params[:page], :per_page => 4)
+  	@djs = Dj.find(15,2,5,1)
   	@gears = Gear.paginate(:page=> params[:page], :per_page => 4)
   end
 end
