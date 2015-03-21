a0a679c4f1ba516f4d789af9e29273ef4081c3a5
supasit <supasit.rit@gmail.com>
2015-03-22 01:08:02 +0700

    lets go migration

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -1,7 +1,7 @@
 class Gear < ActiveRecord::Base
 	has_many :djs_gears
 	has_many :djs, through: :djs_gears
-	has_one :geartype
+	belongs_to :geartype
 	# has_and_belongs_to_many :djs, :join_table => "djs_gears"
 	has_attached_file :avatar, :styles => { 
 	  	:medium => "300x300>", 
--- app/models/geartype.rb
+++ app/models/geartype.rb
@@ -1,2 +1,3 @@
 class Geartype < ActiveRecord::Base
+	has_many :gears
 end
