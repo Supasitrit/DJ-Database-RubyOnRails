01b3c7db2155aff293b06948d142a352c90d3f52
supasit <supasit.rit@gmail.com>
2015-03-21 21:15:41 +0700

    Dj Phillip

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,7 +1,7 @@
 class StaticPageController < ApplicationController
   def home
   	# @djs = Dj.paginate(:page => params[:page], :per_page => 4)
-  	@djs = Dj.find(15,2,5,1)
+  	@djs = Dj.find(1,2,15,16)
   	@gears = Gear.paginate(:page=> params[:page], :per_page => 4)
   end
 end
