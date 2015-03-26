f709bb9621ff7220060d44708a45d93a27b85d5b
Rit <richierit.t@gmail.com>
2015-03-26 22:56:00 +0700

    in the middle of testing

--- app/models/dj.rb
+++ app/models/dj.rb
@@ -1,5 +1,5 @@
 class Dj < ActiveRecord::Base
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, :length => {:maximum => 100 }
 	require 'will_paginate/array'
 	has_many :djs_gears
 	has_many :gears, through: :djs_gears
--- test/models/dj_test.rb
+++ test/models/dj_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class DjTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "should not save dj without name" do
+	  dj = Dj.new
+	  assert_not dj.save,
+	   "Saved the dj without a name"
+  end
+
+  test "should report error" do
+	  # some_undefined_variable is not defined elsewhere in the test case
+	  some_undefined_variable
+	  assert true
+  end
+
+  test 'should have unique name' do
+    dj1 = Dj.create(:name => 'Ruby')
+    assert dj1.valid?, "dj1 was not valid #{dj1.errors.inspect}"
+    
+    dj2 = Dj.new(:name => dj1.name)
+    dj2.valid?
+    assert_not dj2.save, "Saved the dj that isn't unique"
+  end
+
+
+
+  test 'should not add dj with too long name' do
+  	dj = Dj.new(:name => "way toooooooo longgggasdfasdjkf;lajsdf;lkjasd;lfkjas;dlkfjka;lsdkfj;lasdkjf;laskdjf;laksjdf;lakjsdf;jas;dlkfkja;sdklfjka;sldkjfj;asldkjjf;alskdjfl;askdjf;laksdjf;laskdfjasdf;lkj")
+	assert_not dj.save, "Saved long name Dj"  
+  end
+
+
+
 end
