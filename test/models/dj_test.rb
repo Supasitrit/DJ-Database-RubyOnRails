74cc420e700fc2d1ed028fdde5fae7cc8acc843c
Rit <richierit.t@gmail.com>
2015-03-27 00:34:34 +0700

    no time no love

--- test/models/dj_test.rb
+++ test/models/dj_test.rb
@@ -26,13 +26,9 @@ class DjTest < ActiveSupport::TestCase
     assert_not dj2.save, "Saved the dj that isn't unique"
   end
 
-
-
   test 'should not add dj with too long name' do
   	dj = Dj.new(:name => "way toooooooo longgggasdfasdjkf;lajsdf;lkjasd;lfkjas;dlkfjka;lsdkfj;lasdkjf;laskdjf;laksjdf;lakjsdf;jas;dlkfkja;sdklfjka;sldkjfj;asldkjjf;alskdjfl;askdjf;laksdjf;laskdfjasdf;lkj")
 	assert_not dj.save, "Saved long name Dj"  
   end
-
-
 
 end
